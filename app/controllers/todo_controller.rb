c93dfbb5d12768c127d162a681a0c26a2bdad224
Naomi Twineomujuni <naomi.twineomujuni@ousd.us>
2017-11-18 23:21:05 +0000

    Set up 1st database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -11,7 +11,7 @@ class TodoController <ApplicationController
            @todo_description="Spanish 3- Write a paper,in spanish, about a disaster."
            @todo_time_estimate=2
         elsif todo_index=="3"
-           @todo_description="History= Read the packet."
+           @todo_description="History- Read the packet."
            @todo_time_estimate=1.5
         end
     end
