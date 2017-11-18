b4c3600245abe7bc21bbb53e96885e4b58b5a58d
Naomi Twineomujuni <naomi.twineomujuni@ousd.us>
2017-11-18 22:59:27 +0000

    linked all items

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,8 +3,16 @@ class TodoController <ApplicationController
     end
     
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4
+       todo_index= params[:id]
+       if todo_index=="1"
+           @todo_description="English- Oral defense thesis."
+           @todo_time_estimate= 3 
+        elsif todo_index=="2"
+           @todo_description="Spanish 3- Write a paper,in spanish, about a disaster."
+           @todo_time_estimate=2
+        elsif todo_index=="3"
+           @todo_description="History= Read the packet."
+           @todo_time_estimate=1.5
+        end
     end
-    
 end
