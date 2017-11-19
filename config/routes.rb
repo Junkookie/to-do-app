1b44c43db519222f0976910b4c7c38f4ae9d4c68
Naomi Twineomujuni <naomi.twineomujuni@ousd.us>
2017-11-19 22:27:20 +0000

    created the destroy option

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ get 'todo/index', to:'todo#index'
 get 'todo/show/:id', to: 'todo#show'
 get 'todo/edit/:id', to: 'todo#edit'
 get 'todo/update/:id', to: 'todo#update'
+get 'todo/destroy/:id',to: 'todo#destroy'
 end
