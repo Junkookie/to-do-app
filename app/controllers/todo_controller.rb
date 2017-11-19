118850e2ea146c5c0987fd8590f853f6083deaff
Naomi Twineomujuni <naomi.twineomujuni@ousd.us>
2017-11-19 18:41:52 +0000

    replacing ifstatements with ids

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,18 +1,8 @@
 class TodoController <ApplicationController
-    def index
-    end
-    
     def show
-       todo_index= params[:id]
-       if todo_index=="1"
-           @todo_description="English- Oral defense thesis."
-           @todo_time_estimate= 3 
-        elsif todo_index=="2"
-           @todo_description="Spanish 3- Write a paper,in spanish, about a disaster."
-           @todo_time_estimate=2
-        elsif todo_index=="3"
-           @todo_description="History- Read the packet."
-           @todo_time_estimate=1.5
-        end
+       @todo = Todo.find_by_id(params[:id])
+    end
+       
+    def index
     end
 end
